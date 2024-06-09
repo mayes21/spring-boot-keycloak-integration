a955f32fa08baf5d930888232efcbcffbe46efde
amayas <amayas.abb>
2024-06-09 15:52:02 +0200

    WIP secure api using keycloak IAM provider

--- src/main/java/dev/amabb/ResourceController.java
+++ src/main/java/dev/amabb/ResourceController.java
@@ -1,8 +1,20 @@
 package dev.amabb;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/demo")
 public class ResourceController {
 
+    @GetMapping
+    public String hello() {
+        return "Hello from Spring boot & Keycloak";
+    }
+
+//    @GetMapping("/hello-2")
+//    public String hello2() {
+//        return "Hello from Spring boot & Keycloak - ADMIN";
+//    }
 }
